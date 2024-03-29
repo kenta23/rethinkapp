54091682d7388b04fb28bfb6251c74b3de1403aa
= <dreyx0159@gmail.com>
2024-03-29 19:22:38 +0800

    Change runtime export and add log level configuration
    - Export of `runtime` removed from the file
    - Addition of `logLevel: 'debug'` to config object in route handler
    
    Note: The `runtime` export is commented out, but not removed completely.

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,13 +1,18 @@
-import { auth } from "@clerk/nextjs";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 import { getAuth } from "@clerk/nextjs/server";
 
 
-const f = createUploadthing();
- 
-//const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
-
+const f = createUploadthing(
+  {
+    errorFormatter: (err) => {
+      console.log("Error uploading file", err.message);
+      console.log("  - Above error caused by:", err.cause);
+  
+      return { message: err.message };
+    },
+  }
+);
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
