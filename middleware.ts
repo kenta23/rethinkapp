374c70fbba1632a3ae488d465f9709082191a88a
= <dreyx0159@gmail.com>
2024-03-29 13:49:37 +0800

    middleware matcher changes

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,10 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/login", "/login/(.*)", "/register", "/register/(.*)", '/projects', '/api/uploadthing'],
+  publicRoutes: ["/", "/login", "/login/(.*)", "/register", "/register/(.*)", '/projects', '/api/uploadthing/*'],
 });
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  //matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+   matcher: ['/((?!.+\.[\w]+$|_next).)', '/', '/(api|trpc)(.)'],
 };
